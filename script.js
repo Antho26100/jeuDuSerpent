d88839326a1e9ba28f921d73efa4ab6244963f4c
Anthony Taravel <taravel.a@hotmail.fr>
2019-07-28 23:00:14 +0200

    remplacement des mots clefs var par const et let

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 window.onload = function() {
-    //Nous utilisons ici var qui sera changé à la phase de refactoringq
-    var canvasWidth = 900;
-    var canvasHeight = 600;
-    var canvas;
-    var blockSize = 30;//sera la taille d'un block pour notre serpent
-    var ctx;//a pour valeur le contexte
-    var delay = 100;//va permettre de fixer un temps de rafraîchissement
-    var kaa;//objet, instance de Snake
-    var api;
-    var widthInBlocks = canvasWidth/blockSize;//permet d'avoir la largeur en block
-    var heightInBlocks = canvasHeight/blockSize;//idem pour la hauteur
-    var score = 0;
-    var timeout;
+    
+    const canvasWidth = 900;
+    const canvasHeight = 600;
+    const blockSize = 30;
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const widthInBlocks = canvasWidth/blockSize;
+    const heightInBlocks = canvasHeight/blockSize;
+    const centreX = canvasWidth / 2;
+    const centreY = canvasHeight / 2;
+    let delay = 100;
+    let kaa;
+    let api;
+    let score;
+    let timeout;
 
     init();
 
     function init() {
         
-        canvas = document.createElement('canvas');//création de l'élèment canvas
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
         canvas.style.border = "30px solid gray";
@@ -25,7 +26,6 @@ window.onload = function() {
         canvas.style.display = "block";
         canvas.style.backgroundColor = "#ddd";
         document.body.appendChild(canvas);//accroche le canvas au document  
-        ctx = canvas.getContext('2d');//notre canvas doit posséder un contexte, ici en 2d
         kaa = new Snake ([[6,4],[5,4],[4,4]], "right");//instance de Snack avec en paramètre le corps du serpent
         api = new Apple([10,10]);
         score = 0;
@@ -45,7 +45,11 @@ window.onload = function() {
                 do{
                     api.setNewPosition();
                 }
-                while(api.isOnSnake(kaa))
+                while(api.isOnSnake(kaa));
+
+                if(score % 5 == 0){
+                    speedUp();
+                }
             }
             ctx.clearRect(0,0,canvas.width, canvas.height);//suppression du contexte
             drawScore();
@@ -56,23 +60,24 @@ window.onload = function() {
         
     }
 
-    function drawBlock(ctx, position) {//dessine un bloc, prend en argument le contexte et un tableau à 2 index... 
-                                      //..qui correspond à un bloc du serpent
-        var x = position[0] * blockSize;//multiplie la position à l'index 0 par la taille d'un bloc fixé à 30
-        var y = position[1] * blockSize;//multiplie la position à l'index 1 par la taille d'un bloc fixé à 30
-        ctx.fillRect(x, y, blockSize, blockSize);//on dessine ds le contexte un carré à la position x et y et de taille 30sur30
-    }                                              //la succession de nos 3 carrés formeront un rectangle correspondant à notre serpent
+    function speedUp() {
+        delay/=2;
+    }
 
-    function gameOver(){//affiche le Game Over
+    function drawBlock(ctx, position) { 
+                                      
+        const x = position[0] * blockSize;
+        const y = position[1] * blockSize;
+        ctx.fillRect(x, y, blockSize, blockSize);
+    }                                             
+
+    function gameOver(){
         ctx.save();
         ctx.font = "bold 80px sans-serif";
         ctx.fillStyle = "#000";
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
         ctx.strokeStyle = "white";
-        ctx.lineWidth = 5;
-        var centreX = canvasWidth / 2;
-        var centreY = canvasHeight / 2;
         ctx.strokeText("Game Over",centreX,centreY - 180);
         ctx.fillText("Game Over",centreX,centreY - 180);
         ctx.font = "bold 35px sans-serif";
@@ -81,46 +86,42 @@ window.onload = function() {
         ctx.restore();
     }
 
-    function restart() {//permet de recommencer une partie
+    function restart() {
         kaa = new Snake ([[6,4],[5,4],[4,4]], "right");
         api = new Apple([10,10]);
         score = 0;
         clearTimeout(timeout);
+        delay = 100;
         refreshCanvas();
     }
 
-    function drawScore(){//dessine le score
+    function drawScore(){
         ctx.save();
         ctx.font = "bold 200px sans-serif";
         ctx.fillStyle = "gray";
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
-        var centreX = canvasWidth / 2;
-        var centreY = canvasHeight / 2;
         ctx.fillText(score.toString(), centreX, centreY);
         ctx.restore();
     }
     
-
-    //fonction constructeur qui permet d'instancier un objet serpent, prend 1 paramètre qui est le corps du serpent[un tableau]
     function Snake(body, direction){
 
         this.body = body;
         this.direction = direction;
         this.ateApple = false;
-        this.draw = function() { // fonction draw permettant de dessiner les blocs constituant le serpent
-            ctx.save(); //sauvegarde le contexte
+        this.draw = function() { 
+            ctx.save(); 
             ctx.fillStyle = "#ff0000";
-            for(var i = 0; i < this.body.length; i ++){ // nous bouclons sur le corps du serpent afin d'en créer les blocs
-                drawBlock(ctx,this.body[i])//appel à la fonction drawBlock sur chaque index du tableau this.body
+            for(let i = 0; i < this.body.length; i ++){ 
+                drawBlock(ctx,this.body[i])
             }
-            ctx.restore(); //restaure le contexte précédent   
+            ctx.restore();    
         }
 
-        this.advance = function () {//fonction permettant de faire avancer le serpent, sera appelé dans la fonction refreshCanvas
-
-            var nextPosition = this.body[0].slice();//on fait une copie de l'index 0 ac la méthode slice stocké dans nextPosition([6,4])
-            switch(this.direction){ // va nous permettre de tester le contenu de la variable direction
+        this.advance = function () {
+            const nextPosition = this.body[0].slice();
+            switch(this.direction){ 
                 case "left": nextPosition[0] -= 1;
                     break;
                 case "right": nextPosition[0] += 1;
@@ -140,7 +141,7 @@ window.onload = function() {
         };
 
         this.setDirection = function (newDirection){// va permettre de régler une direction
-            var allowedDirection; //va stocker un tableau de directions autorisées à 2 index
+            let allowedDirection; //va stocker un tableau de directions autorisées à 2 index
             switch(this.direction){
                 case "left": 
                 case "right": 
@@ -159,23 +160,23 @@ window.onload = function() {
         }
 
         this.checkCollision = function() {//méthode permettant de savoir si il y a une collision
-            var wallCollision = false;
-            var snakeCollision = false;
-            var head = this.body[0];
-            var rest = this.body.slice(1);
-            var headX = head[0];
-            var headY = head[1];
-            var maxX = widthInBlocks - 1; //permet de définir un bloc maximum sur l'axe des abscisses
-            var minX = 0; // permet de définir un bloc minimum sur l'axe des abscisses
-            var maxY = heightInBlocks - 1; // idem pour les ordonnées
-            var minY = 0;
-            var isNotBetweenHorizontalWall = headX < minX || headX > maxX; //stocke dans cette variable la condition dans laquelle le serpent n'est pas entre les murs horizontaux
-            var isNotBetweenVerticalWall = headY < minY || headY > maxY; // pour les murs verticaux
+            let wallCollision = false;
+            let snakeCollision = false;
+            const head = this.body[0];
+            const rest = this.body.slice(1);
+            const headX = head[0];
+            const headY = head[1];
+            const maxX = widthInBlocks - 1; //permet de définir un bloc maximum sur l'axe des abscisses
+            const minX = 0; // permet de définir un bloc minimum sur l'axe des abscisses
+            const maxY = heightInBlocks - 1; // idem pour les ordonnées
+            const minY = 0;
+            const isNotBetweenHorizontalWall = headX < minX || headX > maxX; //stocke dans cette variable la condition dans laquelle le serpent n'est pas entre les murs horizontaux
+            const isNotBetweenVerticalWall = headY < minY || headY > maxY; // pour les murs verticaux
 
             if(isNotBetweenHorizontalWall || isNotBetweenVerticalWall){
                     wallCollision = true;
             }  
-            for(var i = 0; i < rest.length; i++){ //parcours du tableau rest correspondant au corps du serpent
+            for(let i = 0; i < rest.length; i++){ //parcours du tableau rest correspondant au corps du serpent
                 if(headX === rest[i][0] && headY === rest[i][1]){ //si les pts x et y de notre serpent correspondent à ceux d'un des blocs du serpent, il y a snakeCollision
                         snakeCollision = true;
                 }
@@ -183,7 +184,7 @@ window.onload = function() {
             return wallCollision || snakeCollision;
         }
         this.isEatingApple = function(appleToEat) {//permet de savoir si le serpent à manger la pomme
-            var head = this.body[0];
+            let head = this.body[0];
             if(head[0] === appleToEat.position[0] && head[1] === appleToEat.position[1])
                 return true;
             else 
@@ -197,21 +198,21 @@ window.onload = function() {
             ctx.save();
             ctx.beginPath();
             ctx.fillStyle = "#33cc33";
-            var radius = blockSize/2; //taille du rayon, soit la moitié d'un block
-            var x = this.position[0] * blockSize + radius;//on rajoute radius qui correspond à la moitié d'un bloc afin de se trouver au centre d'un bloc
-            var y = this.position[1] * blockSize + radius;
+            const radius = blockSize/2; //taille du rayon, soit la moitié d'un block
+            const x = this.position[0] * blockSize + radius;//on rajoute radius qui correspond à la moitié d'un bloc afin de se trouver au centre d'un bloc
+            const y = this.position[1] * blockSize + radius;
             ctx.arc(x,y,radius,0,Math.PI*2,true);//méthode afin de créer un cercle
             ctx.fill();
             ctx.restore();
         }
         this.setNewPosition = function() {// permet de paramètrer une nouvelle position à la pomme
-            var newX = Math.round(Math.random() * (widthInBlocks -1));
-            var newY = Math.round(Math.random() * (heightInBlocks -1));
+            const newX = Math.round(Math.random() * (widthInBlocks -1));
+            const newY = Math.round(Math.random() * (heightInBlocks -1));
             this.position = [newX,newY];//tableau contenant la nouvelle position de la pomme
         }
         this.isOnSnake = function (snakeTocheck) {//vérifie que la pomme ne se créer par sur le serpent
-            var isOnSnake = false;
-            for(var i = 0; i < snakeTocheck.body.length; i++){
+            let isOnSnake = false;
+            for(let i = 0; i < snakeTocheck.body.length; i++){
                 if(this.position[0] === snakeTocheck.body[i][0] && this.position[1] === snakeTocheck.body[i][1] ){
                     isOnSnake = true;
                 }
@@ -223,8 +224,8 @@ window.onload = function() {
 
     document.onkeydown = function handleKeyDown(e) { //permet de récupérer un évènement clavier, ici quand l'utilisateur enfonce une touche, capturer dans "e"
         
-        var key = e.keyCode;//on récupère le code de cette touche dans la variable key
-        var newDirection;//stockera la nouvelle direction
+        const key = e.keyCode;//on récupère le code de cette touche dans la variable key
+        let newDirection;//stockera la nouvelle direction
         switch(key){//test les différents cas et stock dans la variable newDirection
             case 37: newDirection = "left";
                 break;
